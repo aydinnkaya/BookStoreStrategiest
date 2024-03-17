e699b1b96b0b404f8ee189bc88041ee3b6b0ebb1
Aydın Kaya <Aydinnkaya1@gmail.com>
2024-03-18 00:52:03 +0300

    Initial commit

---
+++ BookStoreStrategiestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreStrategiestApp.swift
+//  BookStoreStrategiest
+//
+//  Created by Aydın KAYA on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreStrategiestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
